a82edaf3cf7ecd49091f2942bcce0049886dae46
Nico <nicolo.paolini@itsdigitalacademy.com>
2025-06-04 15:25:24 +0200

    bike components

---
+++ src/app/components/bike-list/bike-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-list',
+  templateUrl: './bike-list.component.html',
+  styleUrl: './bike-list.component.scss'
+})
+export class BikeListComponent {
+
+}
