b18124a5b0b3386415c93a3ab4c81aca5289e0a8
Nico <nicolo.paolini@itsdigitalacademy.com>
2025-06-11 17:06:30 +0200

    store

--- src/app/pages/landingpage/landingpage.component.ts
+++ src/app/pages/landingpage/landingpage.component.ts
@@ -1,10 +1,16 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, StoreService } from '../../services/store.service';
 
 @Component({
     selector: 'app-landingpage',
     templateUrl: './landingpage.component.html',
     styleUrls: ['./landingpage.component.scss', '../../app.component.scss'],
 })
-export class LandingpageComponent {
+export class LandingpageComponent implements OnInit {
+    stores: Store[] = [];
 
+    constructor(protected storeSrv: StoreService) { }
+    ngOnInit(): void {
+        this.storeSrv.getStores().subscribe(stores => this.stores = stores);
+    }
 }
