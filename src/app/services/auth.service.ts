fecf649bfe37a707e136c859215b66543bf191e7
Nico <nicolo.paolini@itsdigitalacademy.com>
2025-06-11 22:06:24 +0200

    put dc

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -54,7 +54,7 @@ export class AuthService {
     }
 
     verifyEmail(token: string) {
-        return this.http.get(this.conStr + '/api/auth/verify-email/' + token, { observe: 'response' });
+        return this.http.put(this.conStr + '/api/auth/verify-email/' + token, { observe: 'response' });
     }
 
     private fetchUser() {
